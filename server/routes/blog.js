7198efac7c6538af9879613ebc13fd02c82aaeaa
StJohnErvin <johnervin.ceriola@outlook.com>
2021-01-02 11:01:55 +0800

    validators, user signup/signIn

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,10 +1,8 @@
-const { Router } = require("express");
 const express = require ("express");
 const router = express.Router();
+const {time} = require("../controllers/blog");
 
-router.get("/", (req, res) =>{
-    res.json({time: Date().toString()});
-     })
+router.get("/", time);
 
 
      module.exports=router;
--- server/server.js
+++ server/server.js
@@ -1,49 +1,39 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
- require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
- const blogRoutes= require("./routes/blog")
+// app
+const app = express();
 
+// db
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => {
+        console.log(err);
+    });
 
- //app
-
- const app = express();
-
-//database
-
-mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, useCreateIndex:true, useFindAndModify:false, useUnifiedTopology:true}).then(()=>
-console.log("DATABASE CONNECTED")
-);
-
-
- // middlewares
-
- app.use(morgan("dev"));
- app.use(bodyParser.json());
- app.use(cookieParser());
- 
- //cors
-
-if(process.env.NODE_ENV = "development"){
- app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// cors
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
+// routes middleware
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
-
-
-//routes middle ware
-app.use("/api",blogRoutes);
-
-
-
- //port
-
- const port = process.env.PORT || 8000;
-
- app.listen(port, ( ) =>{
-
-    console.log(`SERVER IS RUNNING ON PORT ${port}`);
- })
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
